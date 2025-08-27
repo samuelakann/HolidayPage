7a2bdba5aa0bab43bc82f49519bee94728a2d1cc
samuelakann <samuellakanni@gmail.com>
2025-08-27 16:33:22 +0100

    updated style, clear comments

--- HolidayPage/src/app/destinations/destinations.page.ts
+++ HolidayPage/src/app/destinations/destinations.page.ts
@@ -25,6 +25,7 @@ import { IonButton } from '@ionic/angular/standalone';
   styleUrls: ['./destinations.page.scss']
 })
 export class DestinationsPage implements OnInit {
+  //variables
   query: string = '';
   results: any[] = [];
   loading: boolean = true;
@@ -32,6 +33,8 @@ export class DestinationsPage implements OnInit {
 
   constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
+  //on init of destination page, get query param, q being searched country name
+  //then call fetchDestinations() to get country data from REST Countries API
   ngOnInit() {
     this.query = this.route.snapshot.queryParamMap.get('q') || '';
     if (this.query) {
@@ -42,6 +45,7 @@ export class DestinationsPage implements OnInit {
     }
   }
 
+  //simple HTTP GET request to REST Countries API to get country info
   fetchDestinations(query: string) {
     this.loading = true;
     this.http.get<any[]>(`https://restcountries.com/v3.1/name/${query}`)
@@ -50,17 +54,19 @@ export class DestinationsPage implements OnInit {
           this.results = data;
           this.loading = false;
         },
-        error: () => {
+        error: () => { //error if country wasnt found, or misspelled
           this.error = 'No results found';
           this.loading = false;
         }
       });
   }
 
+  //adding country to favourites in local storage
   addToFavourites(country: any) {
   let favourites = JSON.parse(localStorage.getItem('favourites') || '[]');
 
   //avoiding duplicates
+  //c is country being added, check if it already exists in favourites, if not, push it to favs
   if (!favourites.find((c: any) => c.name.common === country.name.common)) {
     favourites.push(country);
     localStorage.setItem('favourites', JSON.stringify(favourites));
--- HolidayPage/src/app/home/home.page.ts
+++ HolidayPage/src/app/home/home.page.ts
@@ -6,6 +6,7 @@ import { IonHeader, IonList, IonToolbar, IonTitle, IonContent, IonItem, IonLabel
 import { Geolocation } from '@capacitor/geolocation';
 import { CommonModule } from '@angular/common';
 
+//define country interface to match REST Countries API response
 interface Country {
   name: { common: string; official: string };
   region: string;
@@ -37,43 +38,51 @@ interface Country {
   styleUrls: ['./home.page.scss']
 })
 export class HomePage implements OnInit {
+  //variables
   destination: string = '';
   currentCountry: Country | null = null;
   loadingLocation: boolean = false;
   locationError: string = '';
   favourites: any[] = [];
 
+  //api key for reverse geocoding
   private OPENWEATHER_KEY = 'e06e6dd79dcd1680d8facb99f3660385';
 
   constructor(private router: Router, private http: HttpClient) {}
 
   ngOnInit() {
+    //get user location, load favoutrites
     this.getUserCountry();
     this.loadFavourites();
   }
 
+  //navigating to destination page with search query (country name)
   search() {
     if (this.destination.trim()) {
       this.router.navigate(['/destinations'], { queryParams: { q: this.destination } });
     }
   }
 
+  //load favourites from local storage
   loadFavourites() {
   this.favourites = JSON.parse(localStorage.getItem('favourites') || '[]');
   }
 
+  //open destination page for clicked favourite
   openFavourite(fav: any) {
     this.router.navigate(['/destinations'], { queryParams: { q: fav.name.common } });
-}
+  }
 
+  //getting user country using geolocation and reverse geocoding from OpenWeatherMap
   async getUserCountry() {
     this.loadingLocation = true;
     try {
+      //get user coordinates
       const position = await Geolocation.getCurrentPosition({ enableHighAccuracy: true });
       const lat = position.coords.latitude;
       const lon = position.coords.longitude;
 
-      // OpenWeatherMap reverse geocoding, gets country ISO code from coords, passes to REST Countries API for country name
+      //converting coordinates to country code for the REST Countries API later
       const url = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${this.OPENWEATHER_KEY}`;
       this.http.get<any[]>(url).subscribe({
         next: (data) => {
@@ -86,21 +95,21 @@ export class HomePage implements OnInit {
           }
         }
       });
-    } catch (err) {
+    } catch (err) { //error handling
       console.error('Geolocation error:', err);
       this.locationError = 'Geolocation failed';
       this.loadingLocation = false;
     }
   }
 
+  //fetch country info from REST Countries API using iso code
   fetchCountryInfo(isoCode: string) {
-    // REST Countries API fetches by iso code
     this.http.get<Country[]>(`https://restcountries.com/v3.1/alpha/${isoCode}`).subscribe({
       next: (data) => {
         this.currentCountry = data[0];
         this.loadingLocation = false;
       },
-      error: (err) => {
+      error: (err) => { //error handling if API fails
         console.error('REST Countries API error:', err);
         this.locationError = 'Could not fetch country info';
         this.loadingLocation = false;
